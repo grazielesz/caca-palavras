ec370d907780ace2a3cc818e522e6888e6d89305
Graziele L. de Souza <93684576+grazielesz@users.noreply.github.com>
2022-07-22 09:39:30 -0300

    upload game javascripit, index and style

--- game.js
+++ game.js
@@ -100,8 +100,8 @@ class Palavras {
                             if (this.posicaoPalavra.length == 0 && this.idx + 1 != this.palavra.length) {
                                 this.start(this.idx + 1);
                             } else if (this.posicaoPalavra.length == 0 && this.idx + 1 == this.palavra.length) {
-                                document.getElementById("tempo").innerHTML = "GANHOU";
-                                this.root.innerHTML = "<div id='final'><b>VOCÊ VENCEU!</b><br> Você conseguiu salvar Homer da prisão por sonegação de impostos e pode receber seu prêmio clicando <button id='venceu' onclick='document.location.href = \"https://www.infoescola.com/portugues/oracoes-coordenadas-adversativas\"'>aqui</button></div>"
+                                document.getElementById("tempo").innerHTML = "";
+                                this.root.innerHTML = "<div id='final'><b>VOCÊ VENCEU!</b><br> Você conseguiu salvar Homer da prisão por sonegação de impostos e pode receber seu prêmio clicando <button id='venceu' onclick='document.location.href = \"https://open.spotify.com/playlist/2C0cShMv03xyAXqhu2aaOp?si=2708df87468548b7\"'>aqui</button></div>"
                             }
                         } else if (x.style.backgroundColor != "green") {
                             x.style.backgroundColor = "red";
@@ -157,7 +157,7 @@ function counter_to_x() {
     document.getElementById("timer").innerHTML = `${String(min).padStart(2, "0")}:${String(seg).padStart(2, "0")}`
     segundos++;
     if (segundos == limite) {
-        segundos = 4 * 60;
+        segundos = 3 * 60;
         clearInterval(espera);
         cronos = setInterval(counter_to_0, 1000);
         let timer = document.getElementById("timer");
@@ -176,26 +176,26 @@ let frases = [
     "The Beatles é bom, entretanto, Pink Floyd é melhor",
     "Eu lhe disse, naquela noite, que lhe odiava... Todavia, meu amor, perdido estava em meio a paixão.",
     "Diria-lhe que és o Sol e eu a Lua, contudo, Marta, eu não morreria todas as noites por você.",
-    "Beijou a testa do filho, lhe deu boa noite, porém o observou por alguns segundos, o coração de mãe apertado em vê-lo doente.",
+    "Beijou a testa do filho, lhe deu boa noite, porem o observou por alguns segundos, o coração de mãe apertado em vê-lo doente.",
     "Homer gritou com todo seu ar: \"Amo-te mais do que a lua pode te amar\", mas a verdade Berto sabia, a Lua sempre amaria o Sol.",
     "Maria disse para seu namorado, num fio de voz: \"Se meu pai te pega cá, te mata\", mas seu namorado respondeu: \"que me pegue e me jogue aos porcos, entretanto, ainda a amarei\"",
     "Olhou o corpo do amado coberto de sangue e fechou os olhos, o estômago revirou, todavia, sabia, agora não haveria volta. Era viver ou morrer, porém ele sentia que agora menos ainda vivia.",
-    "Palavras eram impossíveis para ele, sabia disso, contudo, rezava para que seus atos queimassem sob a pele de seu amante, gravando ali que um dia existiu, pois precisava lembrar a si mesmo que não era somente uma alma pecadora, condenada a nada mais que o inferno. Nãoobstante, desejava que sua alma não corrompesse a pura amada, mas apenas ensinasse-a a se amar.",
+    "Palavras eram impossíveis para ele, sabia disso, contudo, rezava para que seus atos queimassem sob a pele de seu amante, gravando ali que um dia existiu, pois precisava lembrar a si mesmo que não era somente uma alma pecadora, condenada a nada mais que o inferno. Naoobstante, desejava que sua alma não corrompesse a pura amada, mas apenas ensinasse-a a se amar.",
     "Ele diria que o que eu fiz foi loucura, contudo,  como ele entenderia quão longe uma mãe iria pelo filho? Ele não sabe, não sabe, porém, no dia que se torne pai, espero que seu coração aguente as horas acordado toda vez que a noite cai e está sozinho em casa.",
     "Marta ainda espera por ele, ainda se senta na cadeira de balanço e espera, mas ela sabe que ele não irá voltar. Na guerra os homens caem, mas sobre seus corpos erguem-se prédios para sustentarem outros homens. Ela só espera que o corpo de seu marido sustente um belo prédio.",
-    "Não obstante suas declarações de amor chegavam as orelhas de sua amada, logo ela deixou de amá-lo e trocou-o por um jogador de futebol flamenguista, quem a julgaria? Até ele se deixaria pelo jogador."
+    "Naoobstante suas declarações de amor chegavam as orelhas de sua amada, logo ela deixou de amá-lo e trocou-o por um jogador de futebol flamenguista, quem a julgaria? Até ele se deixaria pelo jogador."
 ].map(function(x) { return x.toUpperCase() });
 let respostas = [
     "entretanto",
     "todavia",
     "contudo",
-    "porém",
+    "porem",
     "mas",
     "entretanto",
     "todavia",
-    "nãoobstante",
+    "naoobstante",
     "contudo",
     "mas",
-    "nãoobstante"
+    "naoobstante"
 ].map(function(x) { return x.toUpperCase() });
 var palavra = new Palavras(15, frases, respostas, "mae");
